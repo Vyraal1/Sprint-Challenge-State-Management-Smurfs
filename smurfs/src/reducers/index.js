82d1649d7072f1d679365fd681ddfe0bf30b67b5
Vyraal1 <kevin.afable@gmail.com>
2019-10-11 11:34:52 -0400

    dummy data and mapping smurfs

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,5 +1,24 @@
 const initialState = {
-  smurfs: []
+  smurfs: [
+    {
+      name: "Brainey",
+      age: 200,
+      height: "5cm",
+      id: 0
+    },
+    {
+      name: "abyssal whip",
+      age: 2,
+      height: "14cm",
+      id: 1
+    },
+    {
+      name: "trident of the seas",
+      age: 14,
+      height: "7cm",
+      id: 2
+    }
+  ]
 };
 
 const reducer = (state = initialState, action) => {
